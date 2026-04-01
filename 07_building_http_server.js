475dd0dd998ad2acb276e7c769d3906f268cbc40
Devanshi Bhatt <dbhatt2310@gmail.com>
2026-04-01 21:49:45 +0530

    Handle URLs in NodeJS

--- 07_building_http_server.js
+++ 07_building_http_server.js
@@ -1,16 +1,23 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const my_server = http.createServer((req,res) => {
-    const log = `${Date.now()} : New req received\n`;
+    if(req.url === "/favicon.ico") res.end();
+
+    const log = `${Date.now()} : New req received at ${req.url}\n`;
+
+    const my_url = url.parse(req.url, true);
+    console.log(my_url);
 
     fs.appendFile('log.txt', log, (err,data) => {
-        switch(req.url) {
+        switch(my_url.pathname) {
             case '/' : 
                 res.end("homepage");
                 break;
             case '/about' :
-                res.end("hello");
+                const username = my_url.query.myname;
+                res.end(`hello ${username}`);
                 break;
             default : 
                 res.end("404");
