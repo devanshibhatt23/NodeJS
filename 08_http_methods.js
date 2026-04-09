59f04c436633a7247d5a956fd2138c3e53712b4c
Devanshi Bhatt <dbhatt2310@gmail.com>
2026-04-09 22:19:28 +0530

    Add information on http methods

---
+++ 08_http_methods.js
@@ -0,0 +1,9 @@
+// get method - when we enter any url, the browser sends a get request to get data from the server
+
+// post method - when you want to send and mutate/change data in the server
+
+// put - when you want to upload an image/file to the server
+
+// patch - to change an entry in the server
+
+// delete - to delete some data from the server
